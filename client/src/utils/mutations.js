61afc6de452aa7b1edaf7244a2de640d3c8d8f1e
shimna <chimmu.p@gmail.com>
2023-10-13 00:13:45 +1100

    added Apollo useMutation() hook to execute the SAVE_BOOK mutation inside SearchBooks.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -39,21 +39,22 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
 export const REMOVE_BOOK = gql`
-mutation removeBook($bookId:String!){
-    removeBook(bookId:$bookId){
-        _id
-        username
-        email
-        bookCount
-        savedBooks {
-          bookId
-          authors
-          description
-          image
-          link
-          title
-        }
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
       }
     }
-}`;
+  }
+`;
