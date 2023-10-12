b133e40f0e7e075839b492d4167d4fab9067da8d
shimna <chimmu.p@gmail.com>
2023-10-13 00:43:05 +1100

    added useQuery hook to execute the GET_ME in SavedBooks.js

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -63,11 +63,11 @@ const SearchBooks = () => {
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+    // if (!token) {
+    //   return false;
+    // }
 
     try {
       // const response = await saveBook(bookToSave, token);
