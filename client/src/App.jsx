d6fb376efa1939afe5ffc8eee4289eaa2ba97f05
shimna <chimmu.p@gmail.com>
2023-10-12 22:54:40 +1100

    channged the proxy in vite.config and added apollo provider in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,43 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({ uri: "/graphql" });
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <ApolloProvider client={client}>
+        <Navbar />
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
